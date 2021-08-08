b9123ff6fb0cbc607d3385a9175a7caae8a432b0
wmj998 <wmj998@foxmail.com>
2021-08-08 16:44:42 +0800

    Update 热搜词云图.py

--- 热搜词云图.py
+++ 热搜词云图.py
@@ -1,18 +1,19 @@
 import pandas as pd
+import re
 import requests
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from lxml import etree
 
-url = 'https://top.baidu.com/board?tab=realtime&sa=search_31065'
+url = 'https://top.baidu.com/board'
 response = requests.get(url=url)
 html = response.text
 
 selector = etree.HTML(html)
-title = selector.xpath('//*[@id="sanRoot"]/main/div[2]/div/div[2]/div/div[2]/a/text()')
-number = selector.xpath('//*[@id="sanRoot"]/main/div[2]/div/div[2]/div/div[1]/div[2]/text()')
-
-data = pd.Series(data=number, index=title[0:-1:2])
+title = selector.xpath('//*[@id="sanRoot"]/main/div[1]/div[1]/div[2]/a/div[1]/div[2]/div/div/text()')
+number = selector.xpath('//*[@id="sanRoot"]/main/div[1]/div[1]/div[2]/a/div[1]/div[2]/div/span/text()')
+number = [re.findall('\d+', i)[0] for i in number]
+data = pd.Series(data=number, index=title)
 frequencies = data.astype('int')
 
 wordcloud = WordCloud(font_path="FZSTK.TTF",
